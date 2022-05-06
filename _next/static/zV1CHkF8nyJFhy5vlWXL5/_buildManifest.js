ac3a8954a03cab73b67bebaf49d0564dc92903bb
dbtkdok <57377065+dbtkdok@users.noreply.github.com>
2022-05-06 17:38:07 +0900

    Add files via upload

---
+++ _next/static/zV1CHkF8nyJFhy5vlWXL5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F2bb4ab790290f7491614.css","static\u002Fchunks\u002Fpages\u002Findex-8527a040d822a43148bd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
